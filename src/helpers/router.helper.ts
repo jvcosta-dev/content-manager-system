85ff22b993880382798d8a5b28a453ef4b220a8e
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-10-17 15:13:18 -0300

    creating all routes and passing connection to it

---
+++ src/helpers/router.helper.ts
@@ -0,0 +1,24 @@
+import { connectMongo } from "./db.helper";
+import app from "../index";
+import {
+  editContent,
+  getContent,
+  insertContent,
+} from "../controllers/content.controller";
+import { Request, Response } from "express";
+
+export const createRoutes = async (dbName: string, uri: string) => {
+  const connection = await connectMongo(uri, dbName);
+
+  app.get(`/${dbName}`, (req: Request, res: Response) =>
+    getContent(req, res, connection)
+  );
+
+  app.post(`/${dbName}`, (req: Request, res: Response) =>
+    insertContent(req, res, connection)
+  );
+
+  app.patch(`/${dbName}`, editContent);
+
+  console.log(`created routes for /${dbName}`);
+};
